d37226a55edc3aed531d85ad7b15bb1a2df4594f
Samantha Klein <saradurga@gmail.com>
2017-02-13 20:40:42 -0600

    removed extra vars

--- area.js
+++ area.js
@@ -2,14 +2,11 @@ function geometry (x, y, z) {
   var p = (x + y + z) * .5;
   console.log("Perimeter is " + p + ".");
 
-var a = (p-x);
-var b = (p-y);
-var c = (p-z);
-
-var area = Math.sqrt(p * a * b * c);
+var area = Math.sqrt(p * (p-x) * (p-y) * (p-z));
 
 console.log("Area is " + area + ".")
 
 }
 
 geometry(5, 6, 7);
+geometry(2, 3, 4);
